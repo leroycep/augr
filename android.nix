6b987dbdf5442b84e6e72699b5cf87e8e8091d5d
LeRoyce Pearson <leroycepearson@geemili.xyz>
2019-06-28 18:22:40 -0500

    Make `android.nix` more explicit and stylish

--- android.nix
+++ android.nix
@@ -1,10 +1,8 @@
-let native-pkgs = import <nixpkgs> {};
-in
-let pkgs = import <nixpkgs> {
-  crossSystem = native-pkgs.lib.systems.examples.armv7a-android-prebuilt;
-};
-  myrust = (native-pkgs.rustChannels.stable.rust.override {
-    extensions = [ "rust-std" ];
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  native-pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  pkgs = import <nixpkgs> { crossSystem = native-pkgs.lib.systems.examples.armv7a-android-prebuilt; };
+  rust = (native-pkgs.rustChannels.stable.rust.override {
     targets = [
         "armv7-linux-androideabi"
     ];
@@ -13,7 +11,7 @@ in
   pkgs.callPackage (
     {mkShell, cargo}:
     mkShell {
-      nativeBuildInputs = [ myrust ];
+      nativeBuildInputs = [ rust ];
     }
   ) {}
 
