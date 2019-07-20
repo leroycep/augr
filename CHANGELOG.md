a2f7765c57ddcee17265c6bcda0694c26f515a5f
LeRoyce Pearson <leroycepearson@geemili.xyz>
2019-07-20 09:00:39 -0500

    Add `--time` argument to `start` command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Added
+- `--time` argument to `start` command
+
 ## [0.1.1] - 2019-07-18
 ### Fixed
 - Empty device files will no longer cause `augr` to crash
--- cli/src/start.rs
+++ cli/src/start.rs
@@ -1,17 +1,24 @@
-use crate::{database::DataBase, timesheet::Tag};
-use chrono::Utc;
+use crate::{database::DataBase, time_input::parse_default_local, timesheet::Tag};
+use chrono::{DateTime, Local, Utc};
 use std::collections::BTreeSet;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 pub struct StartCmd {
+    /// The time when you started
+    #[structopt(long = "time", parse(try_from_os_str = "parse_default_local"))]
+    time: Option<DateTime<Local>>,
+
     /// A list of tags showing what you are doing
     tags: Vec<String>,
 }
 
 impl StartCmd {
     pub fn exec<DB: DataBase>(&self, db: &mut DB) {
-        let now = Utc::now();
+        let now = self
+            .time
+            .map(|dt| dt.with_timezone(&Utc))
+            .unwrap_or(Utc::now());
         let tags: BTreeSet<Tag> = self.tags.iter().cloned().map(Tag::from).collect();
 
         db.insert_transition(now, tags);
