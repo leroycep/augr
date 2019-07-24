b90eb60b50870c7ea92ed626da9f92f078984f65
LeRoyce Pearson <leroycepearson@geemili.xyz>
2019-07-23 20:17:13 -0500

    Add native gcc to android.nix

--- android.nix
+++ android.nix
@@ -2,16 +2,14 @@ let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   native-pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
   pkgs = import <nixpkgs> { crossSystem = native-pkgs.lib.systems.examples.armv7a-android-prebuilt; };
-  rust = (native-pkgs.rustChannels.stable.rust.override {
-    targets = [
-        "armv7-linux-androideabi"
-    ];
-  });
+  rust = native-pkgs.rustChannelOfTargets "stable" null [
+    "armv7-linux-androideabi"
+  ];
 in
   pkgs.callPackage (
-    {mkShell, cargo}:
+    {mkShell}:
     mkShell {
-      nativeBuildInputs = [ rust ];
+      nativeBuildInputs = [ rust native-pkgs.gcc ];
     }
   ) {}
 
