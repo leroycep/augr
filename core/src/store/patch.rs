f8c746f8ad8184e8f5443893e0f910971b9571f5
LeRoyce Pearson <leroycepearson@geemili.xyz>
2019-08-19 05:48:30 -0500

    Add parents field to actions

--- core/src/store/patch.rs
+++ core/src/store/patch.rs
@@ -31,6 +31,8 @@ pub struct Patch {
 #[derive(Hash, Eq, PartialEq, Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "kebab-case")]
 pub struct AddStart {
+    #[serde(default)]
+    pub parents: Vec<PatchRef>,
     pub parent: PatchRef,
     pub event: EventRef,
     pub time: DateTime<Utc>,
@@ -39,6 +41,8 @@ pub struct AddStart {
 #[derive(Hash, Eq, PartialEq, Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "kebab-case")]
 pub struct RemoveStart {
+    #[serde(default)]
+    pub parents: Vec<PatchRef>,
     pub patch: PatchRef,
     pub event: EventRef,
     pub time: DateTime<Utc>,
@@ -47,6 +51,8 @@ pub struct RemoveStart {
 #[derive(Hash, Eq, PartialEq, Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "kebab-case")]
 pub struct AddTag {
+    #[serde(default)]
+    pub parents: Vec<PatchRef>,
     pub parent: PatchRef,
     pub event: EventRef,
     pub tag: Tag,
@@ -55,6 +61,8 @@ pub struct AddTag {
 #[derive(Hash, Eq, PartialEq, Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "kebab-case")]
 pub struct RemoveTag {
+    #[serde(default)]
+    pub parents: Vec<PatchRef>,
     pub patch: PatchRef,
     pub event: EventRef,
     pub tag: Tag,
@@ -102,12 +110,17 @@ impl Patch {
             .chain(self.remove_start.iter().map(|x| &x.patch))
             .chain(self.add_tag.iter().map(|x| &x.parent))
             .chain(self.remove_tag.iter().map(|x| &x.patch))
+            .chain(self.add_start.iter().flat_map(|x| x.parents.iter()))
+            .chain(self.remove_start.iter().flat_map(|x| x.parents.iter()))
+            .chain(self.add_tag.iter().flat_map(|x| x.parents.iter()))
+            .chain(self.remove_tag.iter().flat_map(|x| x.parents.iter()))
             .cloned()
             .collect()
     }
 
     pub fn add_start(mut self, parent: PatchRef, event: EventRef, time: DateTime<Utc>) -> Self {
         self.add_start.insert(AddStart {
+            parents: Vec::new(),
             parent,
             event,
             time,
@@ -116,17 +129,32 @@ impl Patch {
     }
 
     pub fn remove_start(mut self, patch: PatchRef, event: EventRef, time: DateTime<Utc>) -> Self {
-        self.remove_start.insert(RemoveStart { patch, event, time });
+        self.remove_start.insert(RemoveStart {
+            parents: Vec::new(),
+            patch,
+            event,
+            time,
+        });
         self
     }
 
     pub fn add_tag(mut self, parent: PatchRef, event: EventRef, tag: String) -> Self {
-        self.add_tag.insert(AddTag { parent, event, tag });
+        self.add_tag.insert(AddTag {
+            parents: Vec::new(),
+            parent,
+            event,
+            tag,
+        });
         self
     }
 
     pub fn remove_tag(mut self, patch: PatchRef, event: EventRef, tag: String) -> Self {
-        self.remove_tag.insert(RemoveTag { patch, event, tag });
+        self.remove_tag.insert(RemoveTag {
+            parents: Vec::new(),
+            patch,
+            event,
+            tag,
+        });
         self
     }
 
@@ -138,6 +166,26 @@ impl Patch {
     ) -> Self {
         self.create_event.insert(CreateEvent { event, start, tags });
         self
+    }
+
+    pub fn insert_add_start(&mut self, add_start: AddStart) {
+        self.add_start.insert(add_start);
+    }
+
+    pub fn insert_remove_start(&mut self, remove_start: RemoveStart) {
+        self.remove_start.insert(remove_start);
+    }
+
+    pub fn insert_add_tag(&mut self, add_tag: AddTag) {
+        self.add_tag.insert(add_tag);
+    }
+
+    pub fn insert_remove_tag(&mut self, remove_tag: RemoveTag) {
+        self.remove_tag.insert(remove_tag);
+    }
+
+    pub fn insert_create_event(&mut self, create_event: CreateEvent) {
+        self.create_event.insert(create_event);
     }
 }
 
@@ -170,6 +218,34 @@ mod test {
             event = "a"
             start = "2019-07-24T14:00:00+00:00"
             tags = ["work", "coding"]
+        "#;
+        assert_eq!(toml::de::from_str(toml_str), Ok(expected));
+    }
+
+    #[test]
+    fn read_patch_with_parents() {
+        let id = Uuid::parse_str("e39076fe-6b5a-4a7f-b927-7fc1df5ba275").unwrap();
+        let patch0 = Uuid::parse_str("fa5de1d9-aa11-49fa-b064-8128281a7d91").unwrap();
+        let patch1 = Uuid::parse_str("0c435b19-4504-440c-abc7-f4e4d6a7d25f").unwrap();
+
+        let mut expected = Patch::with_id(id);
+
+        let remove_start = RemoveStart {
+            parents: vec![patch0.clone(), patch1.clone()],
+            patch: patch0.clone(),
+            event: s!("a"),
+            time: Utc.ymd(2019, 7, 24).and_hms(14, 0, 0),
+        };
+        expected.insert_remove_start(remove_start);
+
+        let toml_str = r#"
+            id = "e39076fe-6b5a-4a7f-b927-7fc1df5ba275"
+
+            [[remove-start]]
+            parents = ["fa5de1d9-aa11-49fa-b064-8128281a7d91", "0c435b19-4504-440c-abc7-f4e4d6a7d25f"]
+            patch = "fa5de1d9-aa11-49fa-b064-8128281a7d91"
+            event = "a"
+            time = "2019-07-24T14:00:00+00:00"
         "#;
         assert_eq!(toml::de::from_str(toml_str), Ok(expected));
     }
