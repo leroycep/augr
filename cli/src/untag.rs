7540f32f1ef810e219bb269f8c3b14c6a8b9e7fa
LeRoyce Pearson <leroycepearson@geemili.xyz>
2019-09-19 06:26:21 -0500

    Make removing a non-existant tag with `untag` an error

--- cli/src/untag.rs
+++ cli/src/untag.rs
@@ -1,5 +1,6 @@
 use augr_core::{store::patch::RemoveTag, EventRef, Patch, Timesheet};
 use snafu::Snafu;
+use std::collections::BTreeSet;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -16,6 +17,8 @@ pub struct Cmd {
 pub enum Error {
     #[snafu(display("Unknown event reference: {}", event_ref))]
     UnknownEventRef { event_ref: EventRef },
+    #[snafu(display("Event does not contain the following tags: {:?}", tags))]
+    UnknownTags { tags: BTreeSet<String> },
 }
 
 impl Cmd {
@@ -29,8 +32,10 @@ impl Cmd {
             })?;
         let parent_patches = event.latest_patches();
         let mut patch = Patch::new();
+        let mut tags_to_remove: BTreeSet<_> = self.tags.iter().collect();
         for (patch_ref_added, tag) in event.tags() {
-            if self.tags.contains(&tag) {
+            if tags_to_remove.contains(&tag) {
+                tags_to_remove.remove(&tag);
                 let mut parents = parent_patches.clone();
                 parents.remove(&patch_ref_added);
                 let parents = if parents.is_empty() {
@@ -47,6 +52,15 @@ impl Cmd {
                 });
             }
         }
-        Ok(vec![patch])
+        // If there are any tags left that haven't been removed, the tag doesn't exist
+        // on the event
+        // TODO: figure out how to test this
+        if !tags_to_remove.is_empty() {
+            Err(Error::UnknownTags {
+                tags: tags_to_remove.into_iter().cloned().collect(),
+            })
+        } else {
+            Ok(vec![patch])
+        }
     }
 }
